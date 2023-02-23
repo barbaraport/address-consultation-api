80faf07dff88f04a0152ddeea0768a9ab054c38d
Bárbara Port <62317075+barbaraport@users.noreply.github.com>
2023-02-23 09:46:24 -0300

    creating address dto

---
+++ src/main/java/com/barbaraport/addressConsultationAPI/dto/AddressDTO.java
@@ -0,0 +1,59 @@
+package com.barbaraport.addressConsultationAPI.dto;
+
+public class AddressDTO extends ZipCodeDTO {
+	private String rua;
+	private String complemento;
+	private String bairro;
+	private String cidade;
+	private String estado;
+	private float frete;
+	
+	public String getRua() {
+		return rua;
+	}
+	
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+	
+	public String getComplemento() {
+		return complemento;
+	}
+	
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+	
+	public String getBairro() {
+		return bairro;
+	}
+	
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+	
+	public String getCidade() {
+		return cidade;
+	}
+	
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	
+	public String getEstado() {
+		return estado;
+	}
+	
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	
+	public float getFrete() {
+		return frete;
+	}
+	
+	public void setFrete(float frete) {
+		this.frete = frete;
+	}
+	
+}
