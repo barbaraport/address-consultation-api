0a8045d5473458a00981e17d9766db93cbfe8439
Bárbara Port <62317075+barbaraport@users.noreply.github.com>
2023-02-25 19:58:05 -0300

    the sent zip code can have a mask or not (controller tests)

--- src/test/java/com/barbaraport/addressConsultationAPI/controllers/AddressConsultationControllerTests.java
+++ src/test/java/com/barbaraport/addressConsultationAPI/controllers/AddressConsultationControllerTests.java
@@ -1,6 +1,8 @@
 package com.barbaraport.addressConsultationAPI.controllers;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -23,6 +25,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @AutoConfigureMockMvc
 class AddressConsultationControllerTests {
 	
+	private static final String ROUTE = "/v1/consulta-endereco";
+	
 	@Autowired
     private MockMvc mockMvc;
 
@@ -31,8 +35,8 @@ class AddressConsultationControllerTests {
 	}
 	
 	@Test
-	public void successfulForValidAndExistingZipCode1() throws JsonProcessingException, Exception {
-		MvcResult result = mockMvc.perform(post("/v1/consulta-endereco")
+	public void successfulForValidAndExistingZipCodeSouthEast1WithoutMask() throws JsonProcessingException, Exception {
+		MvcResult result = mockMvc.perform(post(ROUTE)
 		.contentType(MediaType.APPLICATION_JSON)
         .content(new ObjectMapper().writeValueAsString(new ZipCodeDTO("01001000"))))
         .andDo(print())
@@ -53,8 +57,32 @@ class AddressConsultationControllerTests {
 		assertEquals(7.85, address.getFrete());
 	}
 	
-	public void successfulForValidAndExistingZipCode2() throws JsonProcessingException, Exception {
-		MvcResult result = mockMvc.perform(post("/v1/consulta-endereco")
+	@Test
+	public void successfulForValidAndExistingZipCodeSouthEast1WithMask() throws JsonProcessingException, Exception {
+		MvcResult result = mockMvc.perform(post(ROUTE)
+		.contentType(MediaType.APPLICATION_JSON)
+        .content(new ObjectMapper().writeValueAsString(new ZipCodeDTO("01001-000"))))
+        .andDo(print())
+        .andExpect(status().isOk())
+        .andReturn();
+		
+		MockHttpServletResponse response = result.getResponse();
+		String jsonAddress = response.getContentAsString();
+		
+		AddressDTO address = new ObjectMapper().readValue(jsonAddress, AddressDTO.class);
+		
+		assertEquals("01001-000", address.getCep());
+		assertEquals("Praça da Sé", address.getRua());
+		assertEquals("lado ímpar", address.getComplemento());
+		assertEquals("Sé", address.getBairro());
+		assertEquals("São Paulo", address.getCidade());
+		assertEquals("SP", address.getEstado());
+		assertEquals(7.85, address.getFrete());
+	}
+	
+	@Test
+	public void successfulForValidAndExistingZipCodeSouthEast2WithoutMask() throws JsonProcessingException, Exception {
+		MvcResult result = mockMvc.perform(post(ROUTE)
 		.contentType(MediaType.APPLICATION_JSON)
         .content(new ObjectMapper().writeValueAsString(new ZipCodeDTO("12280113"))))
         .andDo(print())
@@ -72,5 +100,203 @@ class AddressConsultationControllerTests {
 		assertEquals("Caçapava", address.getCidade());
 		assertEquals("SP", address.getEstado());
 		assertEquals(7.85, address.getFrete());
+	}
+	
+	@Test
+	public void successfulForValidAndExistingZipCodeSouthEast2WithMask() throws JsonProcessingException, Exception {
+		MvcResult result = mockMvc.perform(post(ROUTE)
+		.contentType(MediaType.APPLICATION_JSON)
+        .content(new ObjectMapper().writeValueAsString(new ZipCodeDTO("12280-113"))))
+        .andDo(print())
+        .andExpect(status().isOk())
+        .andReturn();
+		
+		MockHttpServletResponse response = result.getResponse();
+		String jsonAddress = response.getContentAsString();
+		
+		AddressDTO address = new ObjectMapper().readValue(jsonAddress, AddressDTO.class);		
+		assertEquals("12280-113", address.getCep());
+		assertEquals("Avenida Francisca Salles Damasco", address.getRua());
+		assertEquals("de 641/642 ao fim", address.getComplemento());
+		assertEquals("Jardim São José", address.getBairro());
+		assertEquals("Caçapava", address.getCidade());
+		assertEquals("SP", address.getEstado());
+		assertEquals(7.85, address.getFrete());
+	}
+	
+	@Test
+	public void successfulForValidAndExistingZipCodeNorthEastWithoutMask() throws Exception {
+		MvcResult result = mockMvc.perform(post(ROUTE)
+		.contentType(MediaType.APPLICATION_JSON)
+        .content(new ObjectMapper().writeValueAsString(new ZipCodeDTO("59042250"))))
+        .andDo(print())
+        .andExpect(status().isOk())
+        .andReturn();
+		
+		MockHttpServletResponse response = result.getResponse();
+		String jsonAddress = response.getContentAsString();
+		
+		AddressDTO address = new ObjectMapper().readValue(jsonAddress, AddressDTO.class);	
+		assertEquals("59042-250", address.getCep());
+		assertEquals("Rua Alto da Bela Vista", address.getRua());
+		assertEquals("", address.getComplemento());
+		assertEquals("Nordeste", address.getBairro());
+		assertEquals("Natal", address.getCidade());
+		assertEquals("RN", address.getEstado());
+		assertEquals(15.98, address.getFrete());
+	}
+	
+	@Test
+	public void successfulForValidAndExistingZipCodeNorthEastWithMask() throws Exception {
+		MvcResult result = mockMvc.perform(post(ROUTE)
+		.contentType(MediaType.APPLICATION_JSON)
+        .content(new ObjectMapper().writeValueAsString(new ZipCodeDTO("59042-250"))))
+        .andDo(print())
+        .andExpect(status().isOk())
+        .andReturn();
+		
+		MockHttpServletResponse response = result.getResponse();
+		String jsonAddress = response.getContentAsString();
+		
+		AddressDTO address = new ObjectMapper().readValue(jsonAddress, AddressDTO.class);			
+		assertEquals("59042-250", address.getCep());
+		assertEquals("Rua Alto da Bela Vista", address.getRua());
+		assertEquals("", address.getComplemento());
+		assertEquals("Nordeste", address.getBairro());
+		assertEquals("Natal", address.getCidade());
+		assertEquals("RN", address.getEstado());
+		assertEquals(15.98, address.getFrete());
+	}
+	
+	@Test
+	public void successfulForValidAndExistingZipCodeNorthWithoutMask() throws Exception {
+		MvcResult result = mockMvc.perform(post(ROUTE)
+		.contentType(MediaType.APPLICATION_JSON)
+        .content(new ObjectMapper().writeValueAsString(new ZipCodeDTO("69230970"))))
+        .andDo(print())
+        .andExpect(status().isOk())
+        .andReturn();
+		
+		MockHttpServletResponse response = result.getResponse();
+		String jsonAddress = response.getContentAsString();
+		
+		AddressDTO address = new ObjectMapper().readValue(jsonAddress, AddressDTO.class);			
+		assertEquals("69230-970", address.getCep());
+		assertEquals("Rua 07 de Setembro", address.getRua());
+		assertEquals("100", address.getComplemento());
+		assertEquals("Centro", address.getBairro());
+		assertEquals("Nova Olinda do Norte", address.getCidade());
+		assertEquals("AM", address.getEstado());
+		assertEquals(20.83, address.getFrete());
+	}
+	
+	@Test
+	public void successfulForValidAndExistingZipCodeNorthWithMask() throws Exception {
+		MvcResult result = mockMvc.perform(post(ROUTE)
+		.contentType(MediaType.APPLICATION_JSON)
+        .content(new ObjectMapper().writeValueAsString(new ZipCodeDTO("69230-970"))))
+        .andDo(print())
+        .andExpect(status().isOk())
+        .andReturn();
+		
+		MockHttpServletResponse response = result.getResponse();
+		String jsonAddress = response.getContentAsString();
+		
+		AddressDTO address = new ObjectMapper().readValue(jsonAddress, AddressDTO.class);			
+		assertEquals("69230-970", address.getCep());
+		assertEquals("Rua 07 de Setembro", address.getRua());
+		assertEquals("100", address.getComplemento());
+		assertEquals("Centro", address.getBairro());
+		assertEquals("Nova Olinda do Norte", address.getCidade());
+		assertEquals("AM", address.getEstado());
+		assertEquals(20.83, address.getFrete());
+	}
+	
+	@Test
+	public void successfulForValidAndExistingZipCodeMidWestWithoutMask() throws Exception {
+		MvcResult result = mockMvc.perform(post(ROUTE)
+		.contentType(MediaType.APPLICATION_JSON)
+        .content(new ObjectMapper().writeValueAsString(new ZipCodeDTO("70150900"))))
+        .andDo(print())
+        .andExpect(status().isOk())
+        .andReturn();
+		
+		MockHttpServletResponse response = result.getResponse();
+		String jsonAddress = response.getContentAsString();
+		
+		AddressDTO address = new ObjectMapper().readValue(jsonAddress, AddressDTO.class);			
+		assertEquals("70150-900", address.getCep());
+		assertEquals("Praça dos Três Poderes", address.getRua());
+		assertEquals("", address.getComplemento());
+		assertEquals("Zona Cívico-Administrativa", address.getBairro());
+		assertEquals("Brasília", address.getCidade());
+		assertEquals("DF", address.getEstado());
+		assertEquals(12.50, address.getFrete());
+	}
+	
+	@Test
+	public void successfulForValidAndExistingZipCodeMidWestWithMask() throws Exception {
+		MvcResult result = mockMvc.perform(post(ROUTE)
+		.contentType(MediaType.APPLICATION_JSON)
+        .content(new ObjectMapper().writeValueAsString(new ZipCodeDTO("70150-900"))))
+        .andDo(print())
+        .andExpect(status().isOk())
+        .andReturn();
+		
+		MockHttpServletResponse response = result.getResponse();
+		String jsonAddress = response.getContentAsString();
+		
+		AddressDTO address = new ObjectMapper().readValue(jsonAddress, AddressDTO.class);			
+		assertEquals("70150-900", address.getCep());
+		assertEquals("Praça dos Três Poderes", address.getRua());
+		assertEquals("", address.getComplemento());
+		assertEquals("Zona Cívico-Administrativa", address.getBairro());
+		assertEquals("Brasília", address.getCidade());
+		assertEquals("DF", address.getEstado());
+		assertEquals(12.50, address.getFrete());
+	}
+	
+	@Test
+	public void successfulForValidAndExistingZipCodeSouthWithoutMask() throws Exception {
+		MvcResult result = mockMvc.perform(post(ROUTE)
+		.contentType(MediaType.APPLICATION_JSON)
+        .content(new ObjectMapper().writeValueAsString(new ZipCodeDTO("90040320"))))
+        .andDo(print())
+        .andExpect(status().isOk())
+        .andReturn();
+		
+		MockHttpServletResponse response = result.getResponse();
+		String jsonAddress = response.getContentAsString();
+		
+		AddressDTO address = new ObjectMapper().readValue(jsonAddress, AddressDTO.class);		
+		assertEquals("90040-320", address.getCep());
+		assertEquals("Rua Vieira de Castro", address.getRua());
+		assertEquals("", address.getComplemento());
+		assertEquals("Farroupilha", address.getBairro());
+		assertEquals("Porto Alegre", address.getCidade());
+		assertEquals("RS", address.getEstado());
+		assertEquals(17.30, address.getFrete());
+	}
+	
+	@Test
+	public void successfulForValidAndExistingZipCodeSouthWithMask() throws Exception {
+		MvcResult result = mockMvc.perform(post(ROUTE)
+		.contentType(MediaType.APPLICATION_JSON)
+        .content(new ObjectMapper().writeValueAsString(new ZipCodeDTO("90040-320"))))
+        .andDo(print())
+        .andExpect(status().isOk())
+        .andReturn();
+		
+		MockHttpServletResponse response = result.getResponse();
+		String jsonAddress = response.getContentAsString();
+		
+		AddressDTO address = new ObjectMapper().readValue(jsonAddress, AddressDTO.class);		
+		assertEquals("90040-320", address.getCep());
+		assertEquals("Rua Vieira de Castro", address.getRua());
+		assertEquals("", address.getComplemento());
+		assertEquals("Farroupilha", address.getBairro());
+		assertEquals("Porto Alegre", address.getCidade());
+		assertEquals("RS", address.getEstado());
+		assertEquals(17.30, address.getFrete());
 	}
 }
