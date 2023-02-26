0ee0210f27bc1f9033d737de67f4481b08e4008f
Bárbara Port <62317075+barbaraport@users.noreply.github.com>
2023-02-26 12:19:18 -0300

    adding API and controller documentation

--- src/main/java/com/barbaraport/addressConsultationAPI/controllers/AddressConsultationController.java
+++ src/main/java/com/barbaraport/addressConsultationAPI/controllers/AddressConsultationController.java
@@ -12,13 +12,42 @@ import com.barbaraport.addressConsultationAPI.dto.AddressDTO;
 import com.barbaraport.addressConsultationAPI.dto.ZipCodeDTO;
 import com.barbaraport.addressConsultationAPI.services.AddressConsultationService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 @RestController
-@RequestMapping(path = "v1/consulta-endereco")
+@Api(
+		value = "Address Consultation API",
+		consumes = "application/json",
+		produces = "application/json"
+)
+@RequestMapping(path = "/v1/consulta-endereco")
 public class AddressConsultationController {
 	
 	@Autowired
 	private AddressConsultationService addressConsultationService;
 
+	@ApiOperation(
+			value = "Finds the address from a given zip code",
+			notes = "The zip code can have a mask or not. Example: 12280-112 or 12280112",
+			consumes = "application/json",
+			produces = "application/json",
+			httpMethod = "POST"
+	)
+	@ApiResponses(value = {
+	        @ApiResponse(
+	        		code = 200, 
+	        		message = "Everything is OK and the address was found", 
+	        		response = AddressDTO.class
+	        ),
+	        @ApiResponse(
+	        		code = 400,
+	        		message = "The zip code is invalid or it does not exist",
+	        		response = String.class
+	        ),
+	})
 	@PostMapping(produces = "application/json;charset=UTF-8")
 	public ResponseEntity<?> getAddress(@RequestBody ZipCodeDTO zipCodeDTO) throws Exception {
 		try {
